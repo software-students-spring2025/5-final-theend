33683e9d7bdbaec60c8e3187961405f32f137beb
tmathur2005 <tm3995@nyu.edu>
2025-04-29 14:02:48 -0400

    Update main.py

--- data-service/main.py
+++ data-service/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
