8b20b4287ccc64d08915123e0ebf3b7453669320
naseem-student <nu2061@nyu.edu>
2025-04-29 14:30:38 -0400

    working for digital

--- app.py
+++ app.py
@@ -327,4 +327,5 @@ def reports():
                               error=f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8080)  # DigitalOcean uses port 8080 by default
+
